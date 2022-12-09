ec5878c34f6fdafd1b984b099cf4f0b6840a15fe
Yizhou Zhang <essiazhang@gmail.com>
2022-12-09 13:33:40 -0800

    updated property naming, relationships in user and messages models.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,8 @@
 from .db import db
 from .user import User
-from .channel import Channel
+from .channel import Channel, user_channels
+from .channel_message import ChannelMessage
+from .group_message import GroupMessage
+from .group import Group
+from .user_group import user_groups
 from .db import environment, SCHEMA
