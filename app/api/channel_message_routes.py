9c637aaac98463da16a609af7c027fc2662905a0
XueLan Wu <xuelanwu823@gmail.com>
2022-12-09 20:00:57 -0500

    PUT and DELETE routes added

--- app/api/channel_message_routes.py
+++ app/api/channel_message_routes.py
@@ -12,21 +12,24 @@ channel_message_routes = Blueprint('channel_messages', __name__)
 @login_required
 def get_channel_message(channel_id):
     channel = Channel.query.get(channel_id)
+    print("********************* channel_members",channel.channel_members)
     if current_user in channel.channel_members:
-        print(channel.channnel_members)
-        messages = ChannelMessage.query.filter(user_channels.channel_id == channel_id)
+        print("********************* users_channels",users_channels)
+        messages = ChannelMessage.query.filter(users_channels.channel_id == channel_id)
         return {"channel_messages":[channel_message for channel_message in channel_messages]}
     # user_channel = user_channels.query.filter(user_channels.user_id == current_user.id).filter(user_channels.channel_id == channel_id)
     # print("****************", user_channel)
     # if user_channel:
     #    messages = ChannelMessage.query.filter(user_channels.channel_id == channel_id)
     #    return {"channel_messages":[channel_message for channel_message in channel_messages]}
-    return {"errors": "Only channel member can see the messages"}
+    return {"Errors": "Only channel member can see the messages"}
+    return "1"
 
-@channel_message_routes.route('/<int:channel_id>')
+@channel_message_routes.route('/<int:channel_id>', methods=["POST"])
 @login_required
 def create_channel_message(channel_id):
     channel = Channel.query.get(channel_id)
+    form['csrf_token'].data = request.cookies['csrf_token']
     if current_user in channel.channel_members:
         form = ChannelMessageForm()
         if form.validate_on_submit():
@@ -34,14 +37,31 @@ def create_channel_message(channel_id):
                 db.session.add(new_message)
                 db.session.commit()
                 return new_message.to_dict()
-    return {"errors": "Only channel member can send messages"}
+        if form.errors:
+            print(form.errors)
+    return {"Errors": "Only channel member can send messages"}
 
-@channel_message_routes.route('/<int:id>')
+@channel_message_routes.route('/<int:id>', methods=["PUT"])
 @login_required
 def edit_channel_message(channel_id):
-    pass
+    channel_message = ChannelMessage.query.get(id)
+    form = ChannelMessageForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if channel_message:
+        if form.validate_on_submit():
+            channel_message.content = form.data["content"]
+            db.session.commit()
+            return channel_message.to_dict()
+        if form.errors:
+            print(form.errors)
+    return {"Errors": "The channel_message could not be found"}, 404
 
-@channel_message_routes.route('/<int:id>')
+@channel_message_routes.route('/<int:id>', methods=["DELETE"])
 @login_required
-def delete_channel_message(channel_id):
-    pass
+def delete_channel_message(id):
+    channel_message = ChannelMessage.query.get(id)
+    if channel_message:
+        db.session.delete(channel_message)
+        db.session.commit()
+        return {"Message": "The channel_message was deleted sucessfully!"}, 200
+    return {"Errors": "The channel_message could not be found"}, 404
