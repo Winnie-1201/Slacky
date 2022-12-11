af71f9ceecc41f563cbf78f64ef73c31558e21a8
Nan <nan_guo@hotmail.com>
2022-12-11 15:42:09 -0800

    profile

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -3,10 +3,6 @@ from .signup_form import SignUpForm
 from .channel_form import ChannelForm
 from .channel_message_form import ChannelMessageForm
 from .dm_form import DirectMessageForm
-<<<<<<< HEAD
 from .group_form import GroupForm
 from .userProfile_form import UserProfileForm
-
-=======
 from .userProfile_form import UserProfileForm
->>>>>>> nan-search1
