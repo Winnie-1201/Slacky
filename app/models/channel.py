c786e5700786e8a367f6c768f4a66b5a020e1c35
Yizhou Zhang <essiazhang@gmail.com>
2022-12-11 11:37:21 -0800

    added close menu for profile

--- app/models/channel.py
+++ app/models/channel.py
@@ -48,7 +48,12 @@ class Channel(db.Model):
         return {
             'id': self.id,
             'organizer_id': self.organizer_id,
-            'name': self.name
+            'name': self.name,
+            'description': self.description,
+            'topic': self.topic,
+            'is_public': self.is_public,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
 
     def to_dict(self):
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import LoginGeneral from './LogIn/LoginGeneral';
 
 import './LoginLogout.css'
 
@@ -45,53 +46,56 @@ const SignUpForm = () => {
   }
 
   return (
-    <form onSubmit={onSignUp} className='signup-form'>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div className='signup-form-inputs-div'>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div className='signup-form-inputs-div'>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div className='signup-form-inputs-div'>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div className='signup-form-inputs-div'>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <div className='singup-form-button-div'>
-        <button type='submit'>Sign Up</button>
-      </div>
-    </form>
+    <>
+      <LoginGeneral />
+      <form onSubmit={onSignUp} className='signup-form'>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div className='signup-form-inputs-div'>
+          <label>User Name</label>
+          <input
+            type='text'
+            name='username'
+            onChange={updateUsername}
+            value={username}
+          ></input>
+        </div>
+        <div className='signup-form-inputs-div'>
+          <label>Email</label>
+          <input
+            type='text'
+            name='email'
+            onChange={updateEmail}
+            value={email}
+          ></input>
+        </div>
+        <div className='signup-form-inputs-div'>
+          <label>Password</label>
+          <input
+            type='password'
+            name='password'
+            onChange={updatePassword}
+            value={password}
+          ></input>
+        </div>
+        <div className='signup-form-inputs-div'>
+          <label>Repeat Password</label>
+          <input
+            type='password'
+            name='repeat_password'
+            onChange={updateRepeatPassword}
+            value={repeatPassword}
+            required={true}
+          ></input>
+        </div>
+        <div className='singup-form-button-div'>
+          <button type='submit'>Sign Up</button>
+        </div>
+      </form>
+    </>
   );
 };
 
