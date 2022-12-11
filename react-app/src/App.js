086f5978bac5cc4dd46e5267314eb4af07aae067
XueLan Wu <xuelanwu823@gmail.com>
2022-12-11 13:13:54 -0500

    working on fetching data

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,12 +9,13 @@ import UsersList from "./components/UsersList";
 import User from "./components/User";
 import { authenticate } from "./store/session";
 import SocketTest from "./components/SocketTest";
-import LandingLoggedIn from './components/LandingLoggedIn';
+import LandingLoggedIn from "./components/LandingLoggedIn";
+import ChannelMessagePage from "./components/ChannelMessagePage";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
-  const user = useSelector(state => state.session.user);
+  const user = useSelector((state) => state.session.user);
 
   useEffect(() => {
     (async () => {
@@ -29,17 +30,15 @@ function App() {
 
   return (
     <BrowserRouter>
-      {!user &&
+      {!user && (
         <>
-          <NavBar/>
+          <NavBar />
           <Route path="/" exact={true}>
             <img src="https://previews.us-east-1.widencdn.net/preview/48045879/assets/asset-view/a57a6ee3-fb80-45b4-8b0e-c84837ec5649/thumbnail/eyJ3IjoyMDQ4LCJoIjoyMDQ4LCJzY29wZSI6ImFwcCJ9?Expires=1670788800&Signature=hYtfOYqF4Bf68tSHrI3oxXyd5M~liqCBs9aE2Ch6CUXcIoo2Sv5eaPJSnkDfPW5kzuOFWUn-m1iMFfRG5z0Zvf7Rs3ZG7t4E5e7wmJAjGsmDafHvpkolDgb4ULARMcKaFcWLdTpDgnz69E2thz1Z59nZB2ioVDkMIJK5Cz8LhRBHRLLAh6bfEkG4sKuw8tmv9TG5gkm88GWSe~ktZlpIzGc-EcMyABKYyGD0I51e2VOCvR7KNvUMNTUJNcCKZ-A2QSyh7wV~SxaNOmGDwKzJG9AD1HVlpUqeKNxXCZjZC7TU-4EOu1t0e5EofABUaEWoiLXaFuFFtczJz4gEQeq-iA__&Key-Pair-Id=APKAJM7FVRD2EPOYUXBQ"></img>
           </Route>
         </>
-      }
-      {user &&
-        <LandingLoggedIn user={user}></LandingLoggedIn>
-      }
+      )}
+      {/* {user && <LandingLoggedIn user={user}></LandingLoggedIn>} */}
       <Switch>
         <Route path="/login" exact={true}>
           <LoginForm />
@@ -56,6 +55,9 @@ function App() {
         <Route path="/chat">
           <SocketTest />
         </Route>
+        <ProtectedRoute path="/message/channels/:channelId">
+          <ChannelMessagePage />
+        </ProtectedRoute>
       </Switch>
     </BrowserRouter>
   );
---
+++ react-app/src/components/ChannelMessagePage/index.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchChannelMessages } from "../../store/channelMessage";
+
+const ChannelMessagePage = () => {
+  const channelId = useParams();
+  console.log("**************** channelId in ChannelMessagePage", channelId);
+  const dispatch = useDispatch();
+  const channelMessages = useSelector((state) => state.channelMessages);
+  console.log(channelMessages);
+
+  useEffect(() => {
+    dispatch(fetchChannelMessages(channelId));
+  }, [dispatch, channelId]);
+  if (!channelId) return null;
+  return <div>Hello</div>;
+};
+
+export default ChannelMessagePage;
