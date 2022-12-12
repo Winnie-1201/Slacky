b42a0fa7e9b01a022514710f3d2385af2543c069
Yizhou Zhang <essiazhang@gmail.com>
2022-12-12 12:48:35 -0800

    added prefix for foreignkey, reversed undo order, updated env.py.

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -20,11 +20,11 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_channels()
-        undo_groups()
-        undo_dms()
         undo_seed_channel_messages()
+        undo_dms()
+        undo_groups()
+        undo_channels()
+        undo_users()
 
     seed_users()
     seed_channels()
@@ -37,9 +37,9 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
-    undo_channels()
-    undo_groups()
-    undo_dms()
     undo_seed_channel_messages()
+    undo_dms()
+    undo_groups()
+    undo_channels()
+    undo_users()
     # Add other undo functions here
