08d8665ac142321f657dd864b9877935e42c02cf
XueLan Wu <xuelanwu823@gmail.com>
2022-12-12 17:46:42 -0500

    working on css

--- react-app/src/components/ChannelMessagePage/DisplayBlock/index.js
+++ react-app/src/components/ChannelMessagePage/DisplayBlock/index.js
@@ -1,9 +1,12 @@
 import "./index.css";
 
-const ChannelMessageBlock = ({ cm }) => {
+const ChannelMessageBlock = ({ cm, avatar }) => {
   return (
     <div className="cm-block">
-      <div>{`cm content:       ${cm.content}`}</div>
+      <div className="cm-avatar-box">
+        {avatar && <img src={cm.user.image_url}></img>}
+      </div>
+      <div className="cm-content-box">{`cm content:       ${cm.content}`}</div>
     </div>
   );
 };
--- react-app/src/components/LandingLoggedIn.js
+++ react-app/src/components/LandingLoggedIn.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React from "react";
 import NavBarLoggedIn from "./NavBarLoggedIn";
-import SideBar from './SideBar/SideBar';
-import './LandingLoggedIn.css'
+import SideBar from "./SideBar/SideBar";
+import "./LandingLoggedIn.css";
+import ChannelMessagePage from "./ChannelMessagePage";
 
-export default function LandingLoggedIn({user}) {
+export default function LandingLoggedIn({ user }) {
   return (
-    <div className='landing-grid'>
-        <div className='grid-nav-top'>
-        </div>
-        <div className='grid-nav-top'>
-            <NavBarLoggedIn user={user} />
-        </div>
-        <div className='grid-sidebar'>
-          <SideBar user={user}/>
-        </div>
-        <div className='grid-main-view'>
-
-        </div>
-          
+    <div className="landing-grid">
+      <div className="grid-nav-top">
+        <NavBarLoggedIn user={user} />
+      </div>
+      <div className="grid-sidebar">
+        <SideBar user={user} />
+      </div>
+      <div className="grid-main-view">
+        <ChannelMessagePage />
+      </div>
     </div>
-  )
+  );
 }
