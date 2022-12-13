5e3b157a515e5505be4b2f342e074e93e8e0e8dd
Yizhou Zhang <essiazhang@gmail.com>
2022-12-12 21:54:11 -0800

    formatted create channel form.

--- react-app/src/components/Channels/AddChannel.js
+++ react-app/src/components/Channels/AddChannel.js
@@ -10,20 +10,22 @@ import ChannelModalHeader from './ChannelModalHeader';
 export default function AddChannel({ setShowModal }) {
     const user = useSelector((state) => state.session.user);
     const new_channel = useSelector((state) => state.channels.channel);
-
+    
     const dispatch = useDispatch();
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
-    const [is_public, setIsPublic] = useState(true);
-    const [onSubmit, setOnSubmit] = useState(false);
+    const [isPublic, setIsPublic] = useState(true);
+    const [onFocus, setOnFocus] = useState(false)
     const [disabled, setDisabled] = useState(true);
     const [errors, setErrors] = useState({});
+    const [nameError, setNameError] = useState("Don’t forget to name your channel.");
+    const [descriptionError, setDescriptionError] = useState('');
+
 
     const onCreateChannel = async (e) => {
         e.preventDefault();
-        setOnSubmit(true)
-        if (Object.keys(errors).length) {
-            // console.log('has errors', errors)
+        if (nameError || descriptionError) {
+            // console.log('has errors', nameError, descriptionError)
             return
         }
 
@@ -31,7 +33,7 @@ export default function AddChannel({ setShowModal }) {
         const data = await dispatch(createChannel({
             name,
             description,
-            is_public,
+            isPublic,
             organizer: user,
             users:`${user.id}`
         })).then(() => {
@@ -49,28 +51,47 @@ export default function AddChannel({ setShowModal }) {
     };
 
     useEffect(() => {
-        const validationErros = {}
+        const validationError = {}
 
-        if (name.length <= 0) validationErros.name = "Don’t forget to name your channel.";
-        else if (name.length > 80) validationErros.name = "Channel names can’t be longer than 80 characters.";
-        else delete errors.name
-
-        if (description.length > 250) validationErros.description = "This field can’t be more than 250 characters.";
-        else delete errors.description  
-    
-        // console.log('useEffect, validation error length', Object.keys(validationErros).length, validationErros)
-        if (!Object.keys(validationErros).length) {
-            // console.log('set disabled to false')
-            setDisabled(false)
-        } else {
-            setDisabled(true)
+        if (name.length <= 0) {
+            setNameError("Don’t forget to name your channel.");
+            validationError.name = "Don’t forget to name your channel."
+        } else if (name.length > 80) {
+            setNameError("Channel names can’t be longer than 80 characters.");
+            validationError.name = "Channel names can’t be longer than 80 characters."
         }
-        // console.log('&&&&&&&&&', disabled)
+        else {
+            setNameError("")
+            delete validationError.name
+        }
+
+        if (description.length > 250) {
+            setDescriptionError("This field can’t be more than 250 characters.");
+            validationError.description = "This field can’t be more than 250 characters.";
+        } else {
+            setDescriptionError('')
+            delete validationError.description 
+        }
+    
+        console.log(validationError)
+        if (Object.keys(validationError).length) {
+            console.log('set disabled to true')
+            setDisabled(true)
+        } else {
+            console.log('set disabled to false')
+            setDisabled(false)
+        }
+
     }, [name, description])
 
     if (new_channel) return (
         <Redirect push to={`/channels/${new_channel.id}`}/>
     )
+
+    const handleNameChange = (e) => {
+        setName(e.target.value)
+        setOnFocus(true)
+    }
 
   return (
     <div className='channel-create-div'>
@@ -79,14 +100,15 @@ export default function AddChannel({ setShowModal }) {
         <form className='channel-create-form' onSubmit={onCreateChannel}>
             <div className='create-channel-inputs-div'>
                 <div className="create-channel-labels-div">
-                    <label>Name</label>
-                    {onSubmit && errors.name && <span>{errors.name}</span>}
+                    <label htmlFor='name'>Name
+                        {onFocus && nameError.length > 0 && <span className='channel-form-error-span'>{nameError}</span>}
+                    </label>
                 </div>
 
                 <input
                     type='text'
                     name='name'
-                    onChange={e => setName(e.target.value)}
+                    onChange={handleNameChange}
                     value={name}
                     placeholder='e.g. plan-budget'
                 ></input>
@@ -94,8 +116,11 @@ export default function AddChannel({ setShowModal }) {
             </div>
             <div className='create-channel-inputs-div'>
                 <div className="create-channel-labels-div">
-                    <label>Description <span className='optional-note-span'>(optional)</span></label>
-                    {onSubmit && errors.description && <span>{errors.description}</span>}
+                    <label htmlFor='description'>Description 
+                        <span className='optional-note-span'>(optional)
+                        </span>
+                        {descriptionError.length > 0 && <span className='channel-form-error-span'>{descriptionError}</span>}
+                    </label>
                 </div>
                 <input
                     type='text'
@@ -103,20 +128,32 @@ export default function AddChannel({ setShowModal }) {
                     onChange={e => setDescription(e.target.value)}
                     value={description}
                 ></input>
+                <span className='optional-note-span'>What’s this channel about?</span>
             </div>
             <div className='create-channel-inputs-div'>
                 <div className="create-channel-labels-div">
-                    <label>Make private</label>
+                    <span>Make private</span>
                 </div>
-                <input
-                    type='checkbox'
-                    name='is_public'
-                    onChange={e => setIsPublic(e.target.value)}
-                    value={is_public}
-                ></input>
+                <div className='create-channel-privacy-div'>
+                    {isPublic ?
+                        <span>When a channel is set to private, it can only be viewed or joined by invitation.</span>
+                        :
+                        <span>This can’t be undone. A private channel cannot be made public later on.</span>
+                    }
+                    <label className='privacy-slider-switch'>
+                        <input
+                            type='checkbox'
+                            name='isPublic'
+                            onChange={() => setIsPublic(prev => !prev)}
+                            value={isPublic}
+                            checked={!isPublic}
+                        ></input>
+                        <span className='privacy-slider'></span>
+                    </label>
+                </div>
             </div>
             <div className='create-channel-button-div'>
-                <button type='submit' disabled={disabled}>Create</button>
+                <button className='modal-submit-button' type='submit' disabled={disabled}>Create</button>
                 <div>
                     {errors.backend?.map((error, ind) => (
                         <div key={ind}>{error}</div>
