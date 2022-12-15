f77a3f2e70d5f42f8e58316b1f7e2931a18419f3
Yizhou Zhang <essiazhang@gmail.com>
2022-12-14 18:36:34 -0800

    fixed landing page nav bar

--- react-app/src/components/Channels/DetailsName.js
+++ react-app/src/components/Channels/DetailsName.js
@@ -6,8 +6,8 @@ export default function DetailsName({ setShowModal, channel}) {
   return (
     <div className='channel-details-header'>
         <div className='channel-name-inmodal'>
-          {channel.is_public ? <span>#</span> : <span style={{ 'display': 'flex', 'justifyContent': 'center', 'marginLeft': '5px' }}><i style={{ 'width': '20px' }} className="fa-solid fa-lock"></i></span>}
-          {channel.name}
+          {channel?.is_public ? <span>#</span> : <span style={{ 'display': 'flex', 'justifyContent': 'center', 'marginLeft': '5px' }}><i style={{ 'width': '20px' }} className="fa-solid fa-lock"></i></span>}
+          {channel?.name}
         </div>
         <button className='modal-exit-button' onClick={() => setShowModal(false)}>
           <svg viewBox="0 0 20 20" style={{'width':'20px'}}>
